31341fc06eff5ce09193d0e93176e72b47c5cfc2
Victor Réville <47865059+vreville@users.noreply.github.com>
2021-04-06 13:19:32 +0200

    Update ZDIUtils.py

--- ZDIUtils.py
+++ ZDIUtils.py
@@ -1,4 +1,4 @@
-#    This file is part oAf starAML
+#    This file is part of starAML
 
 #    starAML is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
